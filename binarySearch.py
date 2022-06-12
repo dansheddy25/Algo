52d9008ae0fe2383f5e2fc323f19e52b2ba31cf5
dansheddy25 <philipshedrack5@gmail.com>
2022-06-12 22:49:00 +0100

    updates

--- binarySearch.py
+++ binarySearch.py
@@ -1,24 +1,55 @@
-def binary_search(list, target):
-    first = list[0]
-    last = len(list) - 1
+# def binary_search(list, target):
+#     first = list[0]
+#     last = len(list) - 1
    
 
+#     while first <= last:
+#         midpoint = (first + last) // 2
+#         if list[midpoint] == target:
+#             return midpoint
+#         elif list[midpoint] < target:
+#             first = midpoint + 1
+#         else:
+#             last = midpoint - 1
+#     return None
+
+# def verify(index):
+#     if index is not None:
+#         print("target founnd at index :", index)
+#     else:
+#         print("Target not found in list")
+
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]   
+# result = binary_search(numbers,9)
+# verify(result)
+
+
+from linearSearch import verify
+
+
+def binary_search(list,target):
+    first = list[0]
+    last = len(list) -1
+
     while first <= last:
-        midpoint = (first + last) // 2
-        if list[midpoint] == target:
-            return midpoint
-        elif list[midpoint] < target:
-            first = midpoint + 1
+        mid = (first + last) // 2
+
+        if list[target] == mid :
+            return mid
+        elif list[mid] < target:
+            first = mid + 1
         else:
-            last = midpoint - 1
+            last = mid - 1
     return None
 
-def verify(index):
-    if index is not None:
-        print("target founnd at index :", index)
-    else:
-        print("Target not found in list")
 
+def verify(index):
+    try:
+        if index is not None:
+            print ("this is the index", index)
+    except:
+        print("target not found in list")
 
 numbers = [1,2,3,4,5,6,7,8,9,10]   
 result = binary_search(numbers,9)
